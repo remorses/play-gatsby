6d7e029f4cace5b05a5f8ecbbbb840e65116eb76
remorses <beats.by.morse@gmail.com>
2019-11-11 22:54:39 +0100

    docz-app-gatsby:

--- docz-app-gatsby/.docz/app/imports.js
+++
@@ -1,8 +0,0 @@
-export const imports = {
-  'README.md': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "readme" */ 'README.md'),
-  'docs/index.mdx': () =>
-    import(
-      /* webpackPrefetch: true, webpackChunkName: "docs-index" */ 'docs/index.mdx'
-    ),
-}
