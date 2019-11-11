4b06c2a4443b2c447cbe9f74a98b7316f4d2c7e2
remorses <beats.by.morse@gmail.com>
2019-11-11 21:44:01 +0100

    xx

---
+++ docz-app-gatsby/gatsby-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  plugins: [
+    'gatsby-theme-docz',
+    `gatsby-transformer-sharp`,
+    'gatsby-plugin-styled-components',
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
